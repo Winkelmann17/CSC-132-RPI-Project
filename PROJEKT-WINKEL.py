967c5c1bf5630991c3af1401c1f98f2ce60e612d
Nicholas Winkelmann <winkelmann.wink@gmail.com>
2021-04-26 16:34:21 -0500

    more updated GUI, arrow keys work with GPIO

--- PROJEKT-WINKEL.py
+++ PROJEKT-WINKEL.py
@@ -10,6 +10,7 @@ import RPi.GPIO as GPIO
 from time import sleep
 from tkinter import *
 GPIO.setmode(GPIO.BCM)
+from pynput import keyboard
 from pynput.keyboard import Key, Listener
 
 led = [22, 23, 24, 25, 26, 27]
@@ -17,91 +18,70 @@ GPIO.setup(led, GPIO.OUT)
 
 
 ##Declaring variables equal to the ports they are linked to on the breadboard
-class Game(Frame) :
-    global Key.up
-    global Key.down
-    global Key.left
-    global Key.right
-    Key.up = up
-    Key.down = down
-    Key.left = left
-    Key.right = right
+class Game:
+    global score
+    score = 0
     # the constructor
-    def __init__(self, parent):
-        #calls the frame class
-        Frame.__init__(self, parent)
-    def KeySetup(self, up, down, left, right):
-        self.up = up
-        self.down = down
-        self.left = left
-        self.right = right
-
-    def setupGUI(self):
-        pass
-
-        
-
-    def play(self):
-        self.setupGUI
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Dance Dance Pi-volution!")
+        self.window.geometry('800x600')
+        self.img1 = PhotoImage(file = "ddr arrows(modified).png")
+        self.img2 = PhotoImage(file = "ddr arrows up (gray).png")
+        self.img3 = PhotoImage(file = "ddr arrows left (gray).png")
+        self.img4 = PhotoImage(file = "ddr arrows right (gray).png")
+        self.img1 = self.img1.subsample(5, 5)
+        self.img2 = self.img2.subsample(5, 5)
+        self.img3 = self.img3.subsample(5, 5)
+        self.img4 = self.img4.subsample(5, 5)
+    def displayArrows(self):
+        arrowUP = Label(self.window, image=self.img2)
+        arrowUP.place(x=294, y=20)
+        arrowDOWN = Label(self.window, image=self.img1)
+        arrowDOWN.place(x=394, y=20)
+        arrowLEFT = Label(self.window, image=self.img3)
+        arrowLEFT.place(x=194, y=20)
+        arrowRIGHT = Label(self.window, image=self.img4)
+        arrowRIGHT.place(x=494, y=20)
+    def Score(self):
+        var = StringVar()
+        global label
+        label = Label( self.window, textvariable=var)
+        var.set(str(score))
+        label.pack()
+    def addPoint(Score):
+        score += 1
+        var.set(str(score))
     def show(key):
-    
-        if key == up:
+        if key == Key.up:
+            Game.addPoint(label)
             GPIO.output(24, True)
             sleep(.1)
             GPIO.output(24, False)
-        if key == down:
+        if key == Key.down:
             GPIO.output(27, True)
             sleep(.1)
             GPIO.output(27, False)
-        if key == left:
+        if key == Key.left:
             GPIO.output(25, True)
             sleep(.1)
             GPIO.output(25, False)
-        if key == right:
+        if key == Key.right:
             GPIO.output(26, True)
             sleep(.1)
             GPIO.output(26, False)
-
-
-def show(key):
-    
-    if key == Key.up:
-        GPIO.output(24, True)
-        sleep(.1)
-        GPIO.output(24, False)
-    if key == Key.down:
-        GPIO.output(27, True)
-        sleep(.1)
-        GPIO.output(27, False)
-    if key == Key.left:
-        GPIO.output(25, True)
-        sleep(.1)
-        GPIO.output(25, False)
-    if key == Key.right:
-        GPIO.output(26, True)
-        sleep(.1)
-        GPIO.output(26, False)
-    if key == Key.delete:
-        return False
-with Listener(on_press = show) as listener:
-    listener.join()
-
 #########################################################3
 # Main Program
 ###########################################################
-WIDTH = 800
-HEIGHT = 600
-print("fatass")
-# create the window
-window = Tk()
-window.title("Dance Dance Pi")
+print("check")
 
 # create the GUI
-g = Game(window)
 
+g = Game()
+g.displayArrows()
+g.Score()
 # play
-g.play()
-
+listener = keyboard.Listener(on_press = Game.show)
+listener.start()
 # wait for the window to close
-window.mainloop()
 
