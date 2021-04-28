ac59b728125b71d9f5db87067cfc034792015ff2
Nicholas Winkelmann <winkelmann.wink@gmail.com>
2021-04-28 10:01:52 -0500

    Third version

--- PROJEKT-WINKEL.py
+++ PROJEKT-WINKEL.py
@@ -6,82 +6,66 @@
 
 ## Importing the breadboard GPIO into Python
 ##import keyboard
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 from time import sleep
 from tkinter import *
-GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BCM)
 from pynput import keyboard
 from pynput.keyboard import Key, Listener
 
-led = [22, 23, 24, 25, 26, 27]
-GPIO.setup(led, GPIO.OUT)
+#led = [22, 23, 24, 25, 26, 27]
+#GPIO.setup(led, GPIO.OUT)
 
 
 ##Declaring variables equal to the ports they are linked to on the breadboard
-class Game:
-    global score
-    score = 0
+class Game(Frame) :
     # the constructor
-    def __init__(self):
-        self.window = Tk()
-        self.window.title("Dance Dance Pi-volution!")
-        self.window.geometry('800x600')
-        self.img1 = PhotoImage(file = "ddr arrows(modified).png")
-        self.img2 = PhotoImage(file = "ddr arrows up (gray).png")
-        self.img3 = PhotoImage(file = "ddr arrows left (gray).png")
-        self.img4 = PhotoImage(file = "ddr arrows right (gray).png")
-        self.img1 = self.img1.subsample(5, 5)
-        self.img2 = self.img2.subsample(5, 5)
-        self.img3 = self.img3.subsample(5, 5)
-        self.img4 = self.img4.subsample(5, 5)
-    def displayArrows(self):
-        arrowUP = Label(self.window, image=self.img2)
-        arrowUP.place(x=294, y=20)
-        arrowDOWN = Label(self.window, image=self.img1)
-        arrowDOWN.place(x=394, y=20)
-        arrowLEFT = Label(self.window, image=self.img3)
-        arrowLEFT.place(x=194, y=20)
-        arrowRIGHT = Label(self.window, image=self.img4)
-        arrowRIGHT.place(x=494, y=20)
+    def __init__(self, parent):
+        #calls the frame class
+        Frame.__init__(self, parent)
+    def setupGUI(self):
+        self.pack(fill=BOTH, expand = 1)
+    def play(self):
+        self.setupGUI
+##    def show(key):
+##        if key == Key.up:
+##            GPIO.output(24, True)
+##            sleep(.1)
+##            GPIO.output(24, False)
+##        if key == Key.down:
+##            GPIO.output(27, True)
+##            sleep(.1)
+##            GPIO.output(27, False)
+##        if key == Key.left:
+##            GPIO.output(25, True)
+##            sleep(.1)
+##            GPIO.output(25, False)
+##        if key == Key.right:
+##            GPIO.output(26, True)
+##            sleep(.1)
+##            GPIO.output(26, False)
     def Score(self):
-        var = StringVar()
-        global label
-        label = Label( self.window, textvariable=var)
-        var.set(str(score))
-        label.pack()
-    def addPoint(Score):
-        score += 1
-        var.set(str(score))
-    def show(key):
-        if key == Key.up:
-            Game.addPoint(label)
-            GPIO.output(24, True)
-            sleep(.1)
-            GPIO.output(24, False)
-        if key == Key.down:
-            GPIO.output(27, True)
-            sleep(.1)
-            GPIO.output(27, False)
-        if key == Key.left:
-            GPIO.output(25, True)
-            sleep(.1)
-            GPIO.output(25, False)
-        if key == Key.right:
-            GPIO.output(26, True)
-            sleep(.1)
-            GPIO.output(26, False)
+        global score
+        if key == Key.up or key == Key.down or key == Key.left or key == Key.right:
+            score += 1
+            print (score)
+            return score
 #########################################################3
 # Main Program
 ###########################################################
-print("check")
+WIDTH = 800
+HEIGHT = 600
+# create the window
+window = Tk()
+window.title("Dance Dance Pi")
 
 # create the GUI
+g = Game(window)
 
-g = Game()
-g.displayArrows()
-g.Score()
 # play
-listener = keyboard.Listener(on_press = Game.show)
-listener.start()
+g.play()
+score = 0
+##listener = keyboard.Listener(on_press = Game.show)
+##listener.start()
 # wait for the window to close
-
+window.mainloop()
